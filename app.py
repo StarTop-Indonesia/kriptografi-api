becf4ad1a1cd10810ae01b670a81904ea25b7695
mkhoiqi <mkhoirurrizqi08@gmail.com>
2021-11-22 15:29:29 +0700

    [FEAT] add new variable and update encrypt decrypt flow

--- app.py
+++ app.py
@@ -9,25 +9,64 @@ api = Api(app)
 
 CORS(app)
 
-class Modification(Resource):
+class Cryptography(Resource):
     def get(self):
-        response = {"msg":"Salam dari Binjai"}
+        option = ['modifikasi', 'playfair', 'affine']
+        response = {"msg": option}
         return response
     
     def post(self):
-        type = request.form['type']
-        if type == 'decrypt':
-            raw_text = request.form['raw_text'] 
-            processed_text = raw_text+' decrypted'
-        else:
-            raw_text = request.form['raw_text'] 
-            processed_text = raw_text+' encrypted'
+        # BEGIN::Encryptor function
+        def encryptor(raw_text, option):
+            if option == 'modifikasi':
+                processed_text = raw_text + ' encrypted with {}'.format(option)
+                return processed_text
+            elif option == 'playfair':
+                processed_text = raw_text + ' encrypted with {}'.format(option)
+                return processed_text
+            elif option == 'affine':
+                processed_text = raw_text + ' encrypted with {}'.format(option)
+                return processed_text
+            else:
+                return 'Jenis algoritma tidak tersedia'
+        # END::Encryptor function
 
-        
+        # BEGIN::Decryptor function
+        def decryptor(raw_text, option):
+            if option == 'modifikasi':
+                processed_text = raw_text + ' decrypted with {}'.format(option)
+                return processed_text
+            elif option == 'playfair':
+                processed_text = raw_text + ' decrypted with {}'.format(option)
+                return processed_text
+            elif option == 'affine':
+                processed_text = raw_text + ' decrypted with {}'.format(option)
+                return processed_text
+            else:
+                return 'Jenis algoritma tidak diketahui'
+        # END::Decryptor function
+            
+        # BEGIN::Checker function
+        def checker(raw_text, option, type):
+            if type == 'decrypt':
+                processed_text = decryptor(raw_text,option)
+                return processed_text
+            elif type == 'encrypt':
+                processed_text = encryptor(raw_text, option)
+                return processed_text
+            else:
+                return 'Hanya dapat melakukan enkripsi dan dekripsi'
+        # END::Checker function
+
+        type = request.json['type']
+        option = request.json['option']
+        raw_text = request.json['raw_text']
+        processed_text = checker(raw_text, option, type)
+
         response = {"msg": processed_text}
         return response
 
-api.add_resource(Modification, "/api/modification", methods=["GET","POST"])
+api.add_resource(Cryptography, "/api/cryptography", methods=["GET","POST"])
 
 if __name__ == "__main__":
     app.run(debug=True, port=5005)
